89ee23693e8a5d49e9d94792d875270202f83169
Gabriel Desiderá <gabriel.desidera@usp.br>
2022-06-12 22:36:15 -0300

    Corrige cursor aparecendo

--- LSD/Assets/Scenes/MainMenu/MainMenu.cs
+++ LSD/Assets/Scenes/MainMenu/MainMenu.cs
@@ -8,6 +8,9 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame ()
     {
+        CursorLockMode lockMode;
+        lockMode = CursorLockMode.Locked;
+        Cursor.lockState = lockMode;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
