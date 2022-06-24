e026220ae6b43da1c834dd1b966403b556fbcd93
Gabriel Desiderá <gabriel.desidera@usp.br>
2022-06-23 23:12:05 -0300

    Adiciona segundo puzzle

---
+++ LSD/Assets/Scripts/PadForBiggerBox.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+/// <summary>
+/// Assigned to pressure pads that a box can be dropped on
+/// </summary>
+public class PadForBiggerBox : MonoBehaviour
+{
+    public int padId;
+    int objectsOnPad = 0;
+
+    /// <summary>
+    /// Called whenever a collision occurs with this pad
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other);
+
+        if (other.gameObject.GetComponent<Rigidbody>().isKinematic == false) //If a box is placed...
+        {
+            Debug.Log("Pressure plate ativada!");
+            Transform objectTransform = other.gameObject.GetComponent<Transform>();
+
+            if (objectTransform.localScale.x > 1)
+            {
+            Debug.Log("Pressure plate ativada com o tamanho correto!");
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+            }
+
+        }
+    }
+
+    /// <summary>
+    /// Called when a colliding objects stops colliding with the pad
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerExit(Collider other)
+    {
+        
+            if (other.gameObject.GetComponent<Rigidbody>() != null) //If it is a box that has stopped colliding...
+            {
+                objectsOnPad--; //...then reduce the number of colliding objects...
+               
+            }
+        
+    }
+}
